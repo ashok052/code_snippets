e36b8c7eff5b397cec540373f130552ae93002fc
Ashok Kumar Vadde <ashokkumarvadde@gmail.com>
2020-08-26 09:59:08 +0800

    Facebook problem - Sum of Two numbers in a list

---
+++ Python/DailyInterviewPro/7_TwoSum.py
@@ -0,0 +1,19 @@
+"""
+Hi, here's your problem today. This problem was recently asked by Facebook:
+
+You are given a list of numbers, and a target number k. Return whether or not there are two numbers in the list that add up to k.
+Try to do it in a single pass of the list.
+
+Example:
+Given [4, 7, 1 , -3, 2] and k = 5, 
+return true since 4 + 1 = 5.
+"""
+
+
+def two_sum(list, k):
+    # Fill this in.
+    pass
+
+
+print(two_sum([4, 7, 1, -3, 2], 5))
+# True
