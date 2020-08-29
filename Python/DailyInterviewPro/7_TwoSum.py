619c2969a7421e2d9c08201f8ddd943123222096
Ashok Kumar Vadde <ashokkumarvadde@gmail.com>
2020-08-29 19:21:34 +0800

    New prblems added from Day 8,9,10

--- Python/DailyInterviewPro/7_TwoSum.py
+++ Python/DailyInterviewPro/7_TwoSum.py
@@ -1,11 +1,12 @@
 """
 Hi, here's your problem today. This problem was recently asked by Facebook:
 
-You are given a list of numbers, and a target number k. Return whether or not there are two numbers in the list that add up to k.
+You are given a list of numbers, and a target number k.
+Return whether or not there are two numbers in the list that add up to k.
 Try to do it in a single pass of the list.
 
 Example:
-Given [4, 7, 1 , -3, 2] and k = 5, 
+Given [4, 7, 1 , -3, 2] and k = 5,
 return true since 4 + 1 = 5.
 """
 
