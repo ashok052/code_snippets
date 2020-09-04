b6f102bee944a0117a744466cc412d8f4fc8f8f6
Ashok Kumar Vadde <ashokkumarvadde@gmail.com>
2020-09-04 23:41:21 +0800

    Facebook - undirected graph

---
+++ Python/DailyInterviewPro/17_Graph.py
@@ -0,0 +1,23 @@
+"""
+Hi, here's your problem today. This problem was recently asked by Facebook:
+
+Given an undirected graph, determine if a cycle exists in the graph.
+
+Challenge: Can you solve this in linear time, linear space?
+"""
+
+
+def find_cycle(graph):
+    pass
+
+
+graph = {
+    'a': {'a2': {}, 'a3': {}},
+    'b': {'b2': {}},
+    'c': {}
+}
+print(find_cycle(graph))
+# False
+graph['c'] = graph
+print(find_cycle(graph))
+# True
